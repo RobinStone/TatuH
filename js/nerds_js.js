52d6708827e87c67c723cabe172c45ae4f8dd033
RobinStone <vohuanrok@mail.ru>
2018-07-23 14:18:53 +0300

    page_1 complited

--- js/nerds_js.js
+++ js/nerds_js.js
@@ -1,51 +1,30 @@
-var numb = 1;
-
-$('.switch-btn').on('click', function() {
-    $('.slider-item').addClass('hidden');
-    var scan_id = this.id;
-    $('.' + scan_id).removeClass('hidden');
-    $('.switch-btn').removeClass('active');
-    $(this).addClass('active');
-});
-
-function checker(){
-    numb++;
-    if (numb > 3){numb = 1;}
-$('#slide-'+numb).click();
-}
-
-setInterval(checker, 8000);
-
-$('.template-item').on('focus', function(){
-    $(this).find('div').addClass('visuality');
-});
-
-$('.template-item').on('blur', function(){
-    $(this).find('div').removeClass('visuality');
-});
-
-$('#closeModal').on('click', function(){
-    $('.popap').css('display', 'none');
-    $('.overlay').css('display', 'none');
-});
-
-$('#submitButton').on('click', function(e){
-    e.preventDefault();
-    
-});
-
 $('#popapVisually').on('click', function(e){
     e.preventDefault();
-    $('.popap').css('display', 'flex');
     $('.overlay').css('display', 'block');
+    $('.popap').css('display', 'block');
+    $('input[type="text"]').focus();
 });
 
+$('#closeModal').on('click', hidePopap);
+$('.overlay').on('click', hidePopap);
 
+function hidePopap(){
+    $('.overlay').css('display', 'none');
+    $('.popap').css('display', 'none');
+}
 
+$(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+        if ($('.overlay').css('display')=='block'){
+            hidePopap();
+        }
+    }
+});
 
-
-
-
+$('.switch-btn').on('click', function(){
+    $('.slider-item').removeClass('active');
+    $('.'+this.id).addClass('active');
+});
 
 
 
